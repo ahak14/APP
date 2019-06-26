82925f5a717ecd484897ec9c371623006c5d0194
ahak14 <ah.abbasi.ah@gmail.com>
2019-06-26 14:56:50 +0430

    update

--- src/ui/battleUI/BattleUI.java
+++ src/ui/battleUI/BattleUI.java
@@ -2,17 +2,18 @@ package ui.battleUI;
 
 import javafx.scene.Group;
 import javafx.scene.ImageCursor;
+import javafx.scene.control.Label;
 import javafx.scene.image.*;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import models.GamePlay.Match;
 import models.Unit;
 import ui.ImageLibrary;
-
-import java.awt.*;
 
 public class BattleUI {
 
@@ -26,9 +27,7 @@ public class BattleUI {
     }
 
 
-    public void battleUI() {
-
-        Stage mainStage = new Stage();
+    public void battleUI(Stage mainStage) {
 
         Pane pane = new Pane();
         background = new ImageView(ImageLibrary .Background.getImage());
@@ -69,18 +68,26 @@ public class BattleUI {
 
         leftImageHeader.setLayoutX(-150);
         leftImageHeader.setLayoutY(-80);
-        rightImageHeader.setLayoutX(800);
-        rightImageHeader.setLayoutY(-65);
+        rightImageHeader.setLayoutX(798);
+        rightImageHeader.setLayoutY(-55);
 
         int player1HeroHp = ((Unit) (match.getPlayer1().getHand().getHero())).getHP();
         int player2HeroHp = ((Unit) (match.getPlayer2().getHand().getHero())).getHP();
         Label labelPlayer1HeroHP = new Label(Integer.toString(player1HeroHp));
         Label labelPlayer2HeroHP = new Label(Integer.toString(player2HeroHp));
 
-        labelPlayer1HeroHP.setLocation(100, 100);
-        labelPlayer2HeroHP.setLocation(800, 100);
+        labelPlayer1HeroHP.relocate(180, 75);
+        labelPlayer2HeroHP.relocate(1090, 75);
 
-        headerImage.getChildren().addAll(leftImageHeader, rightImageHeader);
+        labelPlayer1HeroHP.setScaleX(1.5);
+        labelPlayer1HeroHP.setScaleY(1.5);
+        labelPlayer2HeroHP.setScaleX(1.5);
+        labelPlayer2HeroHP.setScaleY(1.5);
+
+        labelPlayer1HeroHP.setTextFill(Color.WHITE);
+        labelPlayer2HeroHP.setTextFill(Color.WHITE);
+
+        headerImage.getChildren().addAll(leftImageHeader, rightImageHeader, labelPlayer1HeroHP, labelPlayer2HeroHP);
         return headerImage;
     }
 }
